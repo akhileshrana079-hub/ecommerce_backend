42e765fa5d1e17ef7683e3aeb40b61d44d20c7c1
Akhilesh Rana <akhileshrana079@gmail.com>
2026-06-17 12:36:08 +0530

    feat: add product search, category filtering, and pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const { get } = require('mongoose');
+const mongoose = require('mongoose');
 const Product = require('../models/Product');
 
 const createProduct = async (req, res) => {
@@ -25,23 +25,33 @@ const createProduct = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
 
-        res.status(200).json({
-            success: true,
-            count: products.length,
-            products
-        });
+    const filter = {};
 
-    } catch (error) {
-
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-
+    if(req.query.keyword){
+        filter.title = {
+            $regex: req.query.keyword,
+            $options: 'i'
+        };
     }
+
+    if(req.query.category){
+        filter.category = req.query.category;
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = 5;
+
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find(filter)
+        .skip(skip)
+        .limit(limit);
+
+    res.status(200).json({
+        success: true,
+        products
+    });
 };
 
 const getSingleProduct = async(req,res)=>{
